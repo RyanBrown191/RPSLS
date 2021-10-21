cec3d60c44b14aa7ac22ef1a41bcd04cdbdba9ff
Ryan Brown <ryan.a.brown191@gmail.com>
2021-10-21 19:49:29 -0400

    Understand Node

---
+++ Game.js
@@ -0,0 +1,145 @@
+"use strict";
+const prompt = require("prompt-sync")()
+
+
+class Game {
+    constructor() {
+        this.playerOne = new Player(" ");
+        this.playerTwo = new Player(" ");
+    }
+
+    runGame(){
+        this.playerInfo();
+
+    }
+
+
+    playerInfo() {
+        
+        let playerOneInfo = prompt("What is your name?");
+        this.playerOne = new Human(playerOneInfo);
+        let playerTwoInfo = prompt("Would you like to play against another person or an AI? Enter Person or AI");
+        if (playerTwoInfo.toLowerCase() === "person") {
+            let playerTwoInfo = prompt("What is the other persons name?");
+            this.playerTwo = new Human(playerTwoInfo);
+        }
+        else if (playerTwoInfo.toLowerCase() === "ai"){
+            this.playerTwo = new AI();
+            alert("You have chosen to play against an AI.");
+        }
+        else {
+            alert("Invalid selection. Try Again");
+        }
+    }
+}
+    // winningGestures() ;{
+    //     if(this.playerOne.chosenGesture === this.playerTwo.chosenGesture){
+    //         alert("It is a tie");
+    //     }
+    //    else{
+    //     switch (this.playerOne.chosenGesture) {
+    //         case "Rock":
+    //             if(this.playerTwo.chosenGesture === "Scissors"){
+    //                 this.playerOne.score++
+    //                 alert("Rock crushes scissors");
+    //             }
+    //             else if(this.playerTwo.chosenGesture === "Lizard"){
+    //                 this.playerOne.score++
+    //                 alert("Rock crushes Lizard");
+    //             }
+    //             else{
+    //                 this.playerTwo.score++
+    //                 alert("Player Two wins this round");
+    //             }
+    //             break;
+    //         case "Paper":
+    //             if(this.playerTwo.chosenGesture === "Rock"){
+    //                 this.playerOne.score++;
+    //                 alert("Paper covers Rock");
+    //             }
+    //             else if(this.playerTwo.chosenGesture === "Scissors"){
+    //                 this.playerOne.score++
+    //                 alert("Scissors cuts Paper");
+    //             }
+    //             else{
+    //                 this.playerTwo.score++;
+    //                 alert("Player Two wins this round");
+    //             }
+    //             break;
+    //         case "Scissors":
+    //             if(this.playerTwo.chosenGesture === "Paper"){
+    //                 this.playerOne.score++;
+    //                 alert("Scissors cuts Paper");
+    //             }
+    //             else if(this.playerTwo.chosenGesture === "Lizard"){
+    //                 this.playerOne.score++;
+    //                 alert("Scissors decapitates Lizard");
+    //             }
+    //             else {
+    //             this.playerTwo.score++;
+    //                 alert("Player Two wins this round");
+    //             }
+    //             break;
+    //         case "Lizard":
+    //             if(this.playerTwo.chosenGesture === "Spock"){
+    //                 this.playerOne.score++;
+    //                 alert("Lizard poisons Spock");
+    //             }
+    //             else if(this.playerTwo.chosenGesture === "Paper"){
+    //                 this.playerOne.score++;
+    //                 alert("Lizard eats Paper");
+    //             }
+    //             else{
+    //                 this.playerTwo.score++;
+    //                 alert("Player Two wins this round");
+    //             }
+    //             break;
+    //         case "Spock":
+    //             if(this.playerTwo.chosenGesture === "Rock"){
+    //                 this.playerOne.score++;
+    //                 alert("Spock vaporizes Rock");
+    //             }
+    //             else if(this.playerTwo.chosenGesture === "Scissors"){
+    //                 this.playerOne.score++;
+    //                 alert("Spock smashes Scissors");
+    //             }
+    //             else{
+    //                 this.playerTwo.score++;
+    //                 alert("Player Two wins this round");
+    //             }
+    //             break;
+    //             default:
+    //             break;
+    //     }
+    // }
+    // }
+
+
+module.exports = Game
+
+
+    class Player {
+        constructor(name) {
+            this.gameGestures = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+            this.score = 0;
+            this.name = name;
+            this.chosenGesture;
+        }
+    }
+    class Human extends Player {
+        constructor(name) {
+            super(name);
+            this.score = 0;
+        }
+    }
+    class AI extends Player {
+        constructor(name) {
+            super(name);
+            this.score = 0;
+        }
+    }
+
+    //figure out gesture array for choices
+    // AI random selection generator of AI
+    //Clean up prompts
+   
---
+++ index.js
@@ -0,0 +1,4 @@
+const Game = require("./Game")
+
+let game1 = new Game()
+game1.runGame()
